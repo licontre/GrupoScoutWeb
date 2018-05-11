a7dcde26fc6c0b9bbe838f77e96f97ac696ac655
DESKTOP-VDTCJUJ\PC <nkph009@gmail.com>
2018-05-11 13:15:22 +0200

    a la hora de ver un evento ahora sale la informacion del evento. esta el boton para apuntarse pero no funiona. se puede comentar un evento.

--- src/java/BackingBeans/ManejadorEventos.java
+++ src/java/BackingBeans/ManejadorEventos.java
@@ -31,11 +31,96 @@ public class ManejadorEventos implements Serializable{
     
     public ManejadorEventos(){
         eventos = new ArrayList<>();
-        eventos.add(new Evento(1L,"Salida a la montaña",new Date(2018,6,18),"Malaga",300,30,"salida a la montaña un dia de verano"));
-        eventos.add(new Evento(2L,"Salida a la playa",new Date(2018,7,28),"Granada",300,30,"salida a la playa un dia de verano"));
-        eventos.add(new Evento(3L,"Salida a la ciudad",new Date(2018,8,8),"Sevilla",300,30,"salida por la ciudad durante todo el dia"));
-        eventos.add(new Evento(4L,"Salida a Madrid",new Date(2018,10,15),"Madrid",300,30,"Excursion a Madrid, a ver la gran capital"));
-        
+        eventos.add(new Evento(1L,"Salida a la montaña",new Date(20180618111111L),"Malaga",300,30,"Alice was beginning to get very tired of sitting by her sister\n" +
+"on the bank, and of having nothing to do:  once or twice she had\n" +
+"peeped into the book her sister was reading, but it had no\n" +
+"pictures or conversations in it, `and what is the use of a book,'\n" +
+"thought Alice `without pictures or conversation?'\n" +
+"\n" +
+"  So she was considering in her own mind (as well as she could,\n" +
+"for the hot day made her feel very sleepy and stupid), whether\n" +
+"the pleasure of making a daisy-chain would be worth the trouble\n" +
+"of getting up and picking the daisies, when suddenly a White\n" +
+"Rabbit with pink eyes ran close by her.\n" +
+"\n" +
+"  There was nothing so VERY remarkable in that; nor did Alice\n" +
+"think it so VERY much out of the way to hear the Rabbit say to\n" +
+"itself, `Oh dear!  Oh dear!  I shall be late!'  (when she thought\n" +
+"it over afterwards, it occurred to her that she ought to have\n" +
+"wondered at this, but at the time it all seemed quite natural);\n" +
+"but when the Rabbit actually TOOK A WATCH OUT OF ITS WAISTCOAT-\n" +
+"POCKET, and looked at it, and then hurried on, Alice started to\n" +
+"her feet, for it flashed across her mind that she had never\n" +
+"before seen a rabbit with either a waistcoat-pocket, or a watch to\n" +
+"take out of it, and burning with curiosity, she ran across the\n" +
+"field after it, and fortunately was just in time to see it pop\n" +
+"down a large rabbit-hole under the hedge."));
+       eventos.add(new Evento(2L,"Salida a la playa",new Date(2018,7,28),"Granada",300,30,"In another moment down went Alice after it, never once\n" +
+"considering how in the world she was to get out again.\n" +
+"\n" +
+"  The rabbit-hole went straight on like a tunnel for some way,\n" +
+"and then dipped suddenly down, so suddenly that Alice had not a\n" +
+"moment to think about stopping herself before she found herself\n" +
+"falling down a very deep well.\n" +
+"\n" +
+"  Either the well was very deep, or she fell very slowly, for she\n" +
+"had plenty of time as she went down to look about her and to\n" +
+"wonder what was going to happen next.  First, she tried to look\n" +
+"down and make out what she was coming to, but it was too dark to\n" +
+"see anything; then she looked at the sides of the well, and\n" +
+"noticed that they were filled with cupboards and book-shelves;\n" +
+"here and there she saw maps and pictures hung upon pegs.  She\n" +
+"took down a jar from one of the shelves as she passed; it was\n" +
+"labelled `ORANGE MARMALADE', but to her great disappointment it\n" +
+"was empty:  she did not like to drop the jar for fear of killing\n" +
+"somebody, so managed to put it into one of the cupboards as she\n" +
+"fell past it."));
+       eventos.add(new Evento(3L,"Salida a la ciudad",new Date(2018,8,8),"Sevilla",300,30,"`Well!' thought Alice to herself, `after such a fall as this, I\n" +
+"shall think nothing of tumbling down stairs!  How brave they'll\n" +
+"all think me at home!  Why, I wouldn't say anything about it,\n" +
+"even if I fell off the top of the house!' (Which was very likely\n" +
+"true.)\n" +
+"\n" +
+"  Down, down, down.  Would the fall NEVER come to an end!  `I\n" +
+"wonder how many miles I've fallen by this time?' she said aloud.\n" +
+"`I must be getting somewhere near the centre of the earth.  Let\n" +
+"me see:  that would be four thousand miles down, I think--' (for,\n" +
+"you see, Alice had learnt several things of this sort in her\n" +
+"lessons in the schoolroom, and though this was not a VERY good\n" +
+"opportunity for showing off her knowledge, as there was no one to\n" +
+"listen to her, still it was good practice to say it over) `--yes,\n" +
+"that's about the right distance--but then I wonder what Latitude\n" +
+"or Longitude I've got to?'  (Alice had no idea what Latitude was,\n" +
+"or Longitude either, but thought they were nice grand words to\n" +
+"say.)"));
+       eventos.add(new Evento(4L,"Salida a Madrid",new Date(2018,10,15),"Madrid",300,30," Presently she began again.  `I wonder if I shall fall right\n" +
+"THROUGH the earth!  How funny it'll seem to come out among the\n" +
+"people that walk with their heads downward!  The Antipathies, I\n" +
+"think--' (she was rather glad there WAS no one listening, this\n" +
+"time, as it didn't sound at all the right word) `--but I shall\n" +
+"have to ask them what the name of the country is, you know.\n" +
+"Please, Ma'am, is this New Zealand or Australia?' (and she tried\n" +
+"to curtsey as she spoke--fancy CURTSEYING as you're falling\n" +
+"through the air!  Do you think you could manage it?)  `And what\n" +
+"an ignorant little girl she'll think me for asking!  No, it'll\n" +
+"never do to ask:  perhaps I shall see it written up somewhere.'\n" +
+"\n" +
+"  Down, down, down.  There was nothing else to do, so Alice soon\n" +
+"began talking again.  `Dinah'll miss me very much to-night, I\n" +
+"should think!'  (Dinah was the cat.)  `I hope they'll remember\n" +
+"her saucer of milk at tea-time.  Dinah my dear!  I wish you were\n" +
+"down here with me!  There are no mice in the air, I'm afraid, but\n" +
+"you might catch a bat, and that's very like a mouse, you know.\n" +
+"But do cats eat bats, I wonder?'  And here Alice began to get\n" +
+"rather sleepy, and went on saying to herself, in a dreamy sort of\n" +
+"way, `Do cats eat bats?  Do cats eat bats?' and sometimes, `Do\n" +
+"bats eat cats?' for, you see, as she couldn't answer either\n" +
+"question, it didn't much matter which way she put it.  She felt\n" +
+"that she was dozing off, and had just begun to dream that she\n" +
+"was walking hand in hand with Dinah, and saying to her very\n" +
+"earnestly, `Now, Dinah, tell me the truth:  did you ever eat a\n" +
+"bat?' when suddenly, thump! thump! down she came upon a heap of\n" +
+"sticks and dry leaves, and the fall was over."));
     }
     
     public List<Evento> getEventos(){      
